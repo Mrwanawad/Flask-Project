5eb4d9d409574732b9a550078b4aa15fbbd2b0ca
mrwanawad <mrwaanawad@gmail.com>
2025-03-10 15:08:10 +0200

    adding test_app.py

---
+++ test_app.py
@@ -0,0 +1,14 @@
+import flask
+import pytest
+from app import app
+
+
+def test_request() :
+    
+    TestResponse = app.test_client().get('/')
+    response_status_code = TestResponse.status_code
+    response_data = TestResponse.data
+    assert response_status_code == 200, 'Response invalid !'
+    assert response_data == b'<h1 style = "color:red;" >Hello World !</h1>'
+    
+    
