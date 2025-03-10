d833f2e8822c383d0b94da7d3d90f8a0ad6bc726
mrwanawad <mrwaanawad@gmail.com>
2025-03-10 16:06:01 +0200

    final commit

--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ def test_request() :
     response_status_code = TestResponse.status_code
     response_data = TestResponse.data
     assert response_status_code == 200, 'Response invalid !'
-    assert response_data == b'<h1 style = "color:red;" >Hello World !</h1>'
+    assert response_data == b'<h1 style = "color:blue;" >Hello World !</h1>'
     
 #actions.yml    
     
